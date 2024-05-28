4ebe6670ab44b1761051e257a426cbbcc246f0c9
Priyankaadev <priyaaswal79@gmail.com>
2024-05-28 20:14:22 +0530

    added lower and upper half

--- src/Components/Sidebar/LowerHalf.tsx
+++ src/Components/Sidebar/LowerHalf.tsx
@@ -1,8 +1,36 @@
-function LowerHalf(){
-return(
-   <div  className="h-[50%] w-full p-4">
-   LowerHalf
-   </div>
-)
+import { AiFillCloud } from "react-icons/ai";
+import { BsDropletFill } from "react-icons/bs";
+import { MdLocationPin } from "react-icons/md";
+
+function LowerHalf() {
+    return (
+        <div className="h-[50%] w-full p-4 flex flex-col justify-between">
+            <div className="flex flex-col">
+                {/* precipitation */}
+                <div className="flex items-center gap-4 my-2">
+                    <div>
+                        <AiFillCloud />
+                    </div>
+                    <div>
+                        Partially cloudy
+                    </div>
+                </div>
+                <div className="flex items-center gap-4 my-4">
+                    <div>
+                        <BsDropletFill />
+                    </div>
+                    <div>
+                        Perc- 10%
+                    </div>
+
+                </div>
+            </div>
+            <div className="flex gap-2 items-center">
+                {/* location */}
+                <div><MdLocationPin /></div>
+                <div>Bengaluru, KA, India</div>
+            </div>
+        </div>
+    )
 }
 export default LowerHalf;
--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -6,6 +6,9 @@ function Sidebar() {
         <div className="basis-[20%] flex flex-col items-center text-black h-full justify-center rounded-bl-3xl rounded-tl-3xl" style={{background: 'rgba(255,255,255,0.81'}}>
         
         <UpperHalf />
+       <div className="h-[1px] w-[90%] bg-[#e9ecef] mt-8 rounded-md">
+
+       </div>
         <LowerHalf />
         
         </div>
