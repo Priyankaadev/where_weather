59c40f13c175bdc149976650005f87386d33813b
Priyankaadev <priyaaswal79@gmail.com>
2024-05-28 12:57:53 +0530

    added axiosinstance

---
+++ src/Config/axiosinstance.ts
@@ -0,0 +1,9 @@
+import axios, {AxiosInstance} from 'axios';
+
+const instance: AxiosInstance = axios.create({
+    baseURL : 'http://api.weatherapi.com/v1',
+    headers : {
+        'Content-Type': 'application/json'
+    }
+});
+export default instance;
