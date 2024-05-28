66201f23d993bcffdb9d85a89fede408db1a8bb2
Priyankaadev <priyaaswal79@gmail.com>
2024-05-28 13:43:30 +0530

    basic tailwind setup done

---
+++ src/Pages/Home.tsx
@@ -0,0 +1,14 @@
+import MainData from "../Components/MainData/MainData";
+import Sidebar from "../Components/Sidebar/Sidebar";
+
+function Home(){
+return (
+    <>
+    <Sidebar />
+    <MainData />
+    </>
+)
+
+}
+
+export default Home;
