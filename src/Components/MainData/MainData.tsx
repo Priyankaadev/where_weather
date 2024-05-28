e6d4f1bf5ca37678c5d410eac2bc0403282c11e1
Priyankaadev <priyaaswal79@gmail.com>
2024-05-28 14:52:25 +0530

    added basic layout for sidebar and maindata

--- src/Components/MainData/MainData.tsx
+++ src/Components/MainData/MainData.tsx
@@ -1,4 +1,8 @@
 function MainData(){
- return (<>MainData</>)
+ return (
+    <div className="basis-[75%] text-black rounded-br-3xl rounded-tr-3xl h-full bg-[#f6f6f8]" >
+        MainData
+    </div>
+ )
 }
 export default MainData;
--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -1,5 +1,8 @@
-function Sidebar(){
-  return  (<>Sidebar</>
-)
-} 
+function Sidebar() {
+    return (
+        <div className="basis-[25%] flex flex-col items-center text-black h-full rounded-bl-3xl rounded-tl-3xl" style={{background: 'rgba(255,255,255,0.81'}}>
+            sidebar
+        </div>
+    )
+}
 export default Sidebar;
